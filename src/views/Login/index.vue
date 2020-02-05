14afeb16003520c782f6431472cc95569ac38099
Johnny-ss <ferrari-488spider@qq.com>
2020-02-05 15:02:50 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        login
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped lang="scss">
+    #login{
+        height: 100vh;
+        background-color: #344a5f;
+    }
+</style>
